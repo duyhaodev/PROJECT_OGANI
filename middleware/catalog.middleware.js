f38633dc00814982fc8f2191eccb9b3e2579940d
dung0mg5 <20110452@student.hcmute.edu.vn>
2025-05-05 13:42:15 +0700

    catalog

--- middleware/catalog.middleware.js
+++ middleware/catalog.middleware.js
@@ -4,7 +4,6 @@ const loadCatalogList = async (req, res, next) => {
   try {
     const catalogList = await Category.find({ status: 'active' }).lean();
     res.locals.catalogList = catalogList; // gán cho biến global của view
-    console.log("📦 Danh mục lấy từ DB:", catalogList);
     next(); // gọi middleware tiếp theo
   } catch (error) {
     console.error("Lỗi khi tải danh sách danh mục:", error);
