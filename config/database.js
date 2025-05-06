46642a3d2770c3bbc4eeaaa3f5ae33d342cdb05c
Nguyễn Duy Hào <121230897+duyhaodev@users.noreply.github.com>
2025-05-06 19:38:14 +0700

    hao sua lai config

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/HKT_Shop_Dev");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected Successful!")
   } catch (error) {
     console.log("Connected Failure")
