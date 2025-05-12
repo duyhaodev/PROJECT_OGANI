4161b776ee82a7cf7285c85acef10311dd906369
Tiến Trương <truongthanhtien2004@gmail.com>
2025-05-13 02:45:16 +0700

    final

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb+srv://root:123@tientruong.ymkk3am.mongodb.net/OGANI?retryWrites=true&w=majority&appName=TienTruong");
     console.log("Connected Successful!")
   } catch (error) {
     console.log("Connected Failure")
