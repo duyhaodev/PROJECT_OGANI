9e45316ccdb9cf7aaee1bf0de934ddb393d0d437
NGUYENHIEPKA <nguyenhiepka910@gmail.com>
2025-10-22 00:53:01 +0700

    Gộp Final_WebSecurity

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ const httpsOptions = {
   cert: fs.readFileSync(path.join(__dirname, 'certs/dev-selfsigned.crt')),
 };
 
-const PORT = 4000;
+const PORT = 3000;
 
 https.createServer(httpsOptions, app).listen(PORT, () => {
   console.log(`HTTPS server running at https://localhost:${PORT}`);
