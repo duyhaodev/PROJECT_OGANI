c3398deb024fc8a7c4bd7c4e53d48a85e8ad4f2f
NGUYENHIEPKA <nguyenhiepka910@gmail.com>
2025-10-21 23:12:36 +0700

    Security Misconfiguration

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/OGANI");
+    await mongoose.connect("mongodb://localhost:27017/projectbhx");
     console.log("Connected Successful!")
   } catch (error) {
     console.log("Connected Failure")
